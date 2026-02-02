43c93e31505bc66167f5df51e76d536d719a345f
mdratul4505 <dev.raul.coder@gmail.com>
2026-02-03 01:39:15 +0600

    added about hhero section

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import AboutHero from '../components/AboutHero';
+import OurStory from '../components/OurStory';
 
 const About = () => {
     return (
         <div>
-           // 
+         <AboutHero></AboutHero>
+         <OurStory></OurStory>
+
+         
         </div>
     );
 };
---
+++ src/components/OurStory.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OurStory = () => {
+    return (
+        <div>
+            ri
+        </div>
+    );
+};
+
+export default OurStory;
